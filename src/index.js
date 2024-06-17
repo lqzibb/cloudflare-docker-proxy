67c01e00e6700b29cb40598855ac33af15552c36
lqzibb <lqzibb@outlook.com>
2024-06-17 23:36:42 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.ksid.top": dockerHub,
+  "quay.ksid.top": "https://quay.io",
+  "gcr.ksid.top": "https://gcr.io",
+  "k8s-gcr.ksid.top": "https://k8s.gcr.io",
+  "k8s.ksid.top": "https://registry.k8s.io",
+  "ghcr.ksid.top": "https://ghcr.io",
+  "cloudsmith.ksid.top": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.ksid.top": dockerHub,
 };
 
 function routeByHosts(host) {
